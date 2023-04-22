4c739533a4bfeb23ab6369af4b24679d8d9e13ab
Paul T <psvmargs@gmail.com>
2023-04-21 20:12:37 -1000

    Update setup.sh
    spacevim

--- setup.sh
+++ setup.sh
@@ -2,6 +2,6 @@ echo setting up codespace dotfiles...
 echo setting up codespace dotfiles > /var/tmp/dotfilesetup
 date >> /var/tmp/dotfilesetup
 
-#TODO: clone spacevim
-#curl -sLf https://spacevim.org/install.sh | bash
+#install spacevim
+curl -sLf https://spacevim.org/install.sh | bash
 

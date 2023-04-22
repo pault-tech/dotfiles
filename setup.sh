ff97817e421ec0e280cda256335ff38dfdb81d53
Paul T <psvmargs@gmail.com>
2023-04-22 16:40:34 +0000

    comment

--- setup.sh
+++ setup.sh
@@ -1,10 +1,13 @@
-        echo setting up codespace dotfiles...
+#sanity check
+echo setting up codespace dotfiles...
 echo setting up codespace dotfiles > /var/tmp/dotfilesetup
 date >> /var/tmp/dotfilesetup
 
 #install spacevim
 curl -sLf https://spacevim.org/install.sh | bash
 
+#clone other GH repos
+#NOTE: requires update to devcontainer.json https://github.com/orgs/community/discussions/36228
 cd ~/
 git clone https://github.com/pault-tech/dotfiles.git
 

f8e584cbbe35ba48f778da9cfda1ba13f6eeda45
Paul T <psvmargs@gmail.com>
2023-04-26 02:57:33 +0000

    rename

--- setup.sh
+++ setup.sh
@@ -10,5 +10,5 @@ curl -sLf https://spacevim.org/install.sh | bash
 #NOTE: requires update to devcontainer.json https://github.com/orgs/community/discussions/36228
 cd ~/
 git clone https://github.com/pault-tech/dotfiles.git
-git clone https://github.com/pault-tech/dotfiles_spacemacs.git
+git clone https://github.com/pault-tech/dotfiles-spacemacs.git
 

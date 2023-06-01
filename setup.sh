6aa129f32c5eca3569daa59c7c967eb541255085
Paul T <pault-tech@github.com>
2023-06-01 22:00:52 +0000

    WIP

--- setup.sh
+++ setup.sh
@@ -3,12 +3,13 @@ echo setting up codespace dotfiles...
 echo setting up codespace dotfiles > /var/tmp/dotfilesetup
 date >> /var/tmp/dotfilesetup
 
+type vim || sudo apt update & sudo apt install -y vim
+
 #install spacevim
 curl -sLf https://spacevim.org/install.sh | bash
 
 #gnu screen
 type screen || sudo apt update & sudo apt install -y screen
-type vim || sudo apt update & sudo apt install -y vim
 
 #clone other GH repos
 #NOTE: requires update to devcontainer.json https://github.com/orgs/community/discussions/36228

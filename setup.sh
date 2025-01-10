6de5993819d84ff3e4134eff4f2f337677b720fc
Paul T <4277512+pault-tech@users.noreply.github.com>
2025-01-10 20:15:39 +0000

    wip

--- setup.sh
+++ setup.sh
@@ -10,13 +10,17 @@ date >> /var/tmp/dotfilesetup
 curl -sLf https://spacevim.org/install.sh | bash
 echo "[[layers]]" >> ~/spacevim.toml
 echo "  name = \"git\"" >> ~/spacevim.toml
-echo "  git_plugin = 'fugitive'" >> ~/spacevim.toml
+echo "  \#git_plugin = 'fugitive'" >> ~/spacevim.toml
+echo "  git_plugin = 'git'" >> ~/spacevim.toml
 echo "[[layers]]" >> ~/spacevim.toml
 echo "  name = \"core\"" >> ~/spacevim.toml
 echo "  enable_smooth_scrolling = false" >> ~/spacevim.toml
+cat ~/spacevim.toml >> ~/.SpaceVim.d/init.toml
 
 #gnu screen if needed
 type screen || sudo apt update && sudo apt install -y screen
+
+echo 'echo "ctrl p codespace stop if using vscode ui"' > ~/.local/bin/css; chmod +x ~/.local/bin/css
 
 #clone other GH repos
 #NOTE: requires update to devcontainer.json https://github.com/orgs/community/discussions/36228
